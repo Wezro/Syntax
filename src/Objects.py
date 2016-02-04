313fe0fd8adfbce57ce9822e94ee99a9212bb6eb
Parker Schmitt <pokia@wezro.com>
2016-02-03 18:19:44 -0600

    Bug fixes

--- src/Objects.py
+++ src/Objects.py
@@ -5,12 +5,18 @@ import Variable
 import Error
 
 
-outputFile = open("../example.css","w")
-
-
-
-
 # processObject parses .syn file contents into a list. The parameter string is one of the lines from the .syn file
+
+global prevString
+global prevName
+global elements
+global element
+
+prevString = ""
+currentString = ""
+elements = []
+prevName = []
+element = -1
 
 def init(): #This is caled to reinitalise the variables before each process.
     global prevString
@@ -22,8 +28,10 @@ def init(): #This is caled to reinitalise the variables before each process.
     currentString = ""
     elements = []
     prevName = []
-    element = None
+    element = -1
 
+
+#Process the given line of the file.
 def process(string):
     global prevString
     global prevName
@@ -33,58 +41,56 @@ def process(string):
     currentIndentation = indentation(string)
     prevIndentation = indentation(prevString)
 
-    if (currentIndentation > prevIndentation): # Previous one must be a parent.
-        if (element == None):
+    if (currentIndentation > prevIndentation): # Previous line must be a parent.
+        if (element == None): #Ignore the first element, as it is invalid.
             element = -1
 
         else:
-            prevName.append(prevString)
+            prevName.append(prevString) # Register the previous string as a new parent element.
             if (len(elements) > 0):
                 elements[element]["children"] = listRemove(1,elements[element]["children"]) # Delete this parent from the children of the previous parent.
-            elements.append({"name": " ".join(prevName), "children":[]})
+            elements.append({"name": " ".join(prevName), "children":[]}) # Put the prev string (the parent) in the list of elements.
             element = lastIndex(elements)
 
 
 
-    elif (currentIndentation < prevIndentation): # This means we are not a child of the previous parent(s).
+    elif (currentIndentation < prevIndentation): # This means we are not a child of the previous parent(s), and are going down a single or multiple parent.
         # Remove the previous parent(s), and go down a certain amount of elements
         element -= prevIndentation-currentIndentation
-        for i in 0,prevIndentation-currentIndentation:
-            prevName.pop()
+        for i in 0,prevIndentation-currentIndentation: # Delete however many elements we went back (counted via indentations)
+            if (len(prevName) != 0):
+                prevName.pop()
 
 
-    if (len(elements) > 0): # With the way this system works, we have to add every real line to the children of the previous element.
+    if (len(elements) > 0): # With the way this parser works, we have to add every line to the children of the previous element.
         elements[element]["children"].append(string)
 
-
-    #print "---------------------------------"
-    #print elements
-
-    prevString  = string
+    prevString  = string # At the end of each cycle, this sets the previous string
 
 # Exports the elements to a CSS file
 def export(cssPath):
     cssFile = open(cssPath,"w")
-    for element in elements:
-        cssFile.write(element["name"] + "{")
+    for element in elements: # Go through each element, which is the parents.
+        cssFile.write(element["name"] + "{") # Write the parent, followed by a { bracket
 
-        for child in element["children"]:
-            cssFile.write(child + ";")
+        for child in element["children"]: # Go through each child in the elements children.
+            cssFile.write(child + ";") # Write the children (variables)
 
-        cssFile.write("}")
+        cssFile.write("}") # Close the parent.
     cssFile.close()
 
 
 
 #------ Tidbits ------#z
 def combineList(sourceList):
-    for sourceIndex, sourceElement in enumerate(sourceList, start=0):
-        for copyIndex, copyElement in enumerate(sourceList, start=0):
-            if sourceElement["name"] == copyElement["name"]:
+    for sourceIndex, sourceElement in enumerate(sourceList, start=0): # Start looping through the first list
+        for copyIndex, copyElement in enumerate(sourceList, start=0): # Star looping through the same list, this allows us to compare each element in the list to each other.
+            if sourceElement["name"] == copyElement["name"]: # If either of the two have the same name, that means we should delete the copy, and take the children from the copy into the original.
                 sourceList[sourceIndex]["children"] += sourceList[copyIndex]["children"]
                 sourceList.pop(copyIndex)
 
     return sourceList
+
 
 def lastIndex(list): # Returns the last index of a given list.
     return len(list)-1
@@ -93,7 +99,7 @@ def indentation(string): # Returns the number of intentions in a string
     return len(string) - len(string.lstrip())
 
 def strip(string): # Strips indents and linebreaks from a string.
-    return string.lstrip().strip("\n").strip(" ")
+    return string.lstrip().strip("\n").strip(" ").strip("\t")
 
 def listRemove(amount,list): # Removes (n) amount of items from a list, starting from the end.
     return list[:-amount]
--- src/Syntax.py
+++ src/Syntax.py
@@ -11,25 +11,28 @@ syntaxFiles = []
 workingDirectory = None
 
 
-def execute(file):
+def execute(fileName):
     ProcessManager.init()
-    with open(file,"r") as syntaxLines:
+    with open(fileName,"r") as syntaxLines:
         for line in syntaxLines:
+            print(line)
             if not line.strip(): #Skip empty lines
                 continue
             else:
                 ProcessManager.process(line)
 
     #Objects.elements = Objects.combineList(Objects.elements) # This makes sure if there are any duplicates elements, they are combined.
-    Objects.export(file.strip(".syntax") + ".css")
+    print(fileName.strip(".syntax").strip(".syn") + ".css")
+    Objects.export(fileName.replace(".syntax","").replace(".syn","") + ".css")
     Error.showErrors() #Return the errors
+    syntaxLines.close()
 
 def getFiles(folder):
     global syntaxFiles
     syntaxFiles = []
     for fileName in os.listdir(folder):
         if fileName.endswith(".syn") or fileName.endswith(".syntax") :
-            filePath = folder + "\\" + fileName # Have to add a extra slash, othwises Python thinks we are trying to escape the double quote.
+            filePath = folder + fileName # Have to add a extra slash, othwises Python thinks we are trying to escape the double quote.
             syntaxFiles.append({"path": filePath, "time": os.stat(filePath).st_mtime})
 
 
@@ -52,17 +55,12 @@ def getInput():
         elif (args[0] == "watch"):
             print "Watching for changes. Press (CTRL-C) to exit."
             while True:
+                time.sleep(1)
                 for file in syntaxFiles:
                     if (file["time"] != os.stat(file["path"]).st_mtime): # If the times are different, that means the folder has been updated
-                        execute(file["path"])
                         file["time"] = os.stat(file["path"]).st_mtime
-
-
-
-    getInput() #Get the users input again, incase they want to do somthing else.
-
-
-
+                        execute(file["path"])
+    getInput() #Get the users input again, incase the user wants to do somthing else.
 
 
 try:
